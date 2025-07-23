cd653437ec15b17c2045ecb2d6422808b39d7f02
nianob <103071967+nianob@users.noreply.github.com>
2025-07-23 22:18:18 +0200

    Testing new Autoupdate logic

--- bot.py
+++ bot.py
@@ -97,7 +97,7 @@ def text_input(title: str, label: str, placeholder: str = "", default: str = "",
         return Modal
     return decorator
 
-@text_input("A1", "B1", "C1")
+@text_input("A2", "B2", "C2")
 async def response(self, interaction: discord.Interaction):
     await interaction.response.send_message(self.input.value, ephemeral=True)
 
