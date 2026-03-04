0a16d5aab0c1108bcd2e093d5d659891a5954190
nianob <nianobMc@gmail.com>
2026-03-04 23:37:13 +0100

    Add update info, remove requiring being in the same talk for /swarmfm leave, because it's bugged and small update to automod.py

--- automod.py
+++ automod.py
@@ -1,33 +1,12 @@
 import discord
 import regex
 import datetime
-from typing import Optional, Callable
+from typing import Callable
 from customtypes import Storage
+from utils import text_input
 
 save_storage: Callable # This should be overwritten by the importing script
 storage: Storage # This should be overwritten by the importing script
-
-def text_input(title: str, label: str, placeholder: str = "", default: str = "", min_length: Optional[int] = None, max_length: Optional[int] = None, style: discord.TextStyle = discord.TextStyle.short):
-    """
-    Example usage:
-    ```
-    @text_input("Example Title", "Example Field")
-    async def response(interaction: discord.Interaction, reply: str):
-        await interaction.response.send_message(f"You wrote `{reply}`")
-    ```
-    """
-    def decorator(func):
-        class Modal(discord.ui.Modal):
-            def __init__(self):
-                super().__init__(title=title, timeout=300)
-                self.input = discord.ui.TextInput(label=label, placeholder=placeholder, default=default, required=bool(min_length), min_length=min_length, max_length=max_length, style=style)
-                self.add_item(self.input)
-        
-            async def on_submit(self, interaction: discord.Interaction):
-                await func(interaction, self.input.value)
-
-        return Modal
-    return decorator
 
 class AddRuleButton(discord.ui.Button):
     def __init__(self, modal: Callable[[], discord.ui.Modal]):
